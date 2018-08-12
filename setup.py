25fc26dd75bb0bd715aaf9d62adba67e62e15514
Quinn Parrott <parrottq@users.noreply.github.com>
2018-08-12 15:59:12 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='fastpac',
+        description='Arch Linux package downloader',
+        version='0.1',
+        author='Quinn Parrott',
+        url='https://github.com/parrottq/fastpac',
+        packages=['fastpac'],
+        install_requires=['requests'],
+        license='GPL3'
+        )
